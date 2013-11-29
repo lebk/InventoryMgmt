df01dac64da9e2639d9a0c3d1e08934849add726
lebk <terry.lei@symantec.com>
2013-11-29 13:39:46 +0800

    Initial check in for product in

--- src/main/java/com/lebk/action/ProductAction.java
+++ src/main/java/com/lebk/action/ProductAction.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-import com.lebk.dto.ProductDTO;
-import com.lebk.po.Product;
 import com.lebk.services.ProductService;
 import com.lebk.services.impl.ProductServiceImpl;
 import com.opensymphony.xwork2.ActionSupport;
@@ -16,10 +14,38 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class ProductAction extends ActionSupport
 {
-  private List<ProductDTO> productDtoList;
 
   static Logger logger = Logger.getLogger(ProductAction.class);
   ProductService ps = new ProductServiceImpl();
+
+  private List<ProductInRow> productInList;
+
+  private static Integer listSize = 10;
+
+  public List<ProductInRow> getProductInList()
+  {
+    constructProductInList();
+    return productInList;
+  }
+
+  public void setProductInList(List<ProductInRow> productInList)
+  {
+    this.productInList = productInList;
+  }
+
+  private void constructProductInList()
+  {
+    productInList = new ArrayList<ProductInRow>();
+    for (int i = 0; i < listSize; i++)
+    {
+      ProductInRow row = new ProductInRow(i, "产品类型", "产品花色", "产品尺寸", 99, 0);
+      logger.info("the row is:" + row);
+      logger.info("the row is:" + row);
+      productInList.add(row);
+      logger.info("the row is:" + row);
+    }
+
+  }
 
   public String gotoProductIn()
   {
@@ -41,38 +67,88 @@ public class ProductAction extends ActionSupport
     return "productOut";
   }
 
-  public List<ProductDTO> getProductDtoList()
+  class ProductInRow
   {
-    return productDtoList;
-  }
+    private Integer id;
+    private String ptType;
+    private String ptColor;
+    private String ptSize;
+    private Integer ptNumber;
+    private Integer InNum;
 
-  public void setProductDtoList(List<ProductDTO> productDtoList)
-  {
-    this.productDtoList = productDtoList;
-  }
-
-  public String showProductList()
-  {
-
-    productDtoList = new ArrayList<ProductDTO>();
-    logger.info("calling showProductList");
-    List<Product> pl = ps.getAllProductList();
-    convertProductListToProductDTOList(pl, productDtoList);
-    for (Product p : pl)
+    public ProductInRow(int id, String ptType, String ptColor, String ptSize, int ptNumber, int inNum)
     {
-      logger.info("showProductList:" + p.getName());
+      this.id = Integer.valueOf(id);
+      this.ptType = ptType;
+      this.ptColor = ptColor;
+      this.ptSize = ptSize;
+      this.ptNumber = Integer.valueOf(ptNumber);
+      this.InNum = Integer.valueOf(inNum);
     }
-    logger.info("There are:" + productDtoList.size() + " in productDtoList");
-    return "showProductList";
 
-  }
-
-  private void convertProductListToProductDTOList(List<Product> pl, List<ProductDTO> ProductDtoList)
-  {
-    for (Product p : pl)
+    public Integer getId()
     {
-      productDtoList.add(new ProductDTO(p));
+      return id;
+    }
+
+    public void setId(Integer id)
+    {
+      this.id = id;
+    }
+
+    public String getPtType()
+    {
+      return ptType;
+    }
+
+    public void setPtType(String ptType)
+    {
+      this.ptType = ptType;
+    }
+
+    public String getPtColor()
+    {
+      return ptColor;
+    }
+
+    public void setPtColor(String ptColor)
+    {
+      this.ptColor = ptColor;
+    }
+
+    public String getPtSize()
+    {
+      return ptSize;
+    }
+
+    public void setPtSize(String ptSize)
+    {
+      this.ptSize = ptSize;
+    }
+
+    public Integer getPtNumber()
+    {
+      return ptNumber;
+    }
+
+    public void setPtNumber(Integer ptNumber)
+    {
+      this.ptNumber = ptNumber;
+    }
+
+    public Integer getInNum()
+    {
+      return InNum;
+    }
+
+    public void setInNum(Integer inNum)
+    {
+      InNum = inNum;
+    }
+
+    public String toString()
+    {
+      return this.getId() + ":" + this.getPtType() + ":" + this.getPtColor() + ":" + this.getPtSize() + ":" + this.getPtNumber() + ":" + this.getInNum();
     }
   }
-
 }
--- src/main/webapp/pages/productIn.jsp
+++ src/main/webapp/pages/productIn.jsp
@@ -1,25 +1,62 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
 
+<link href="<s:url value="/css/main.css"/>" rel="stylesheet"
+	type="text/css" />
 <html>
 <head>
-<link href="<s:url value="/css/main.css"/>" rel="stylesheet" type="text/css"/>
+<title>产品入库</title>
 <style type="text/css" rel="stylesheet">
-#container{width:500px; margin:10px auto;}
-#container table{text-align:center;width:100%}
-#container #link{text-align:right;}
+#container {
+	width: 80%;
+	margin: 10px auto;
+}
+
+#container table {
+	text-align: center;
+	width: 80%
+}
+
+#container #link {
+	text-align: right;
+}
 </style>
+
 </head>
 <body>
-
-<center>
-<h3>入库单</h3>
-<div id="container">
-<div id="link">
-<s:url action="showProductListtilesAction.action" var="aURL" />
-<s:a href="%{aURL}"><u>[返回首页]</u></s:a>
-</div>
-</div>
-</center>
+	<center>
+		<h3>产品入库</h3>
+		<div id="container">
+			<div id="link">
+				<s:url action="addNewProductColortilesAction.action" var="aURL" />
+				<s:a href="%{aURL}">
+					<u>[提交]</u>
+				</s:a>
+			</div>
+			
+		<table align=center class="borderAll">
+			<tr>
+				<th><s:text name="编号" /></th>
+				<th><s:text name="类型" /></th>
+				<th><s:text name="花色" /></th>
+				<th><s:text name="尺寸" /></th>
+				<th><s:text name="库存数量" /></th>
+				<th><s:text name="入库数量" /></th>
+				<th>&nbsp;</th>
+			</tr>
+			<s:iterator value="productInList" status="status">
+				<tr
+					class="<s:if test="#status.even">even</s:if><s:else>odd</s:else>">
+					<td class="nowrap"><s:property value="id" /></td>
+					<td class="nowrap"><s:property value="ptType" /></td>
+					<td class="nowrap"><s:property value="ptColor" /></td>
+					<td class="nowrap"><s:property value="ptSize" /></td>
+					<td class="nowrap"><s:property value="ptNumber" /></td>
+					<td class="nowrap"><s:textfield name="inNum" size="5" theme="simple"/></td>
+				</tr>
+			</s:iterator>
+		</table>
+		</div>
+	</center>
 </body>
 </html>
